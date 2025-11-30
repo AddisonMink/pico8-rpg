3f79246274123808563b770f02281172734cae39
Addison Mink <addison.mink@example.com>
2025-11-29 18:14:39 -0600

    add camera scrolling in world.

--- world.lua
+++ world.lua
@@ -6,13 +6,21 @@ function world_new()
   -- #endregion
 
   -- #region state
-  local x, y = 64, 64
+  local x, y = 13 * 8, 15 * 8
+  local camera_x, camera_y = x - 32, y - 32
   local moving = false
   local frames = side_frames
   local flip_x = false
   local id = frames[1]
+  local steps = 0
+  local on_scripted_battle_tile = false
+  local dialogue = nil
   local me = {}
   -- #endregion
+
+  local dialogue_map = {
+    ["14,15"] = dialogue_town_shop
+  }
 
   local function tile_pos(x, y)
     return flr((x + 4) / 8), flr((y + 4) / 8)
@@ -23,13 +31,14 @@ function world_new()
     local dy = dx == 0 and (btn(2) and -1 or btn(3) and 1 or 0) or 0
     local tx, ty = tile_pos(x + dx, y + dy)
     local tile_id = mget(tx, ty)
-    local in_bounds = tx >= 0 and tx < 16 and ty > 2 and ty < 16
+    local in_bounds = tx >= 0 and tx < 31 and ty > 2 and ty < 31
 
     if not in_bounds then
       dx, dy = 0, 0
     end
 
     moving = dx != 0 or dy != 0
+    steps += moving and 1 or 0
     x += dx
     y += dy
     if dx ~= 0 then
@@ -41,19 +50,45 @@ function world_new()
     end
 
     id = moving and frames[flr(time() * 6) % #frames + 1] or frames[1]
+
+    if (x - camera_x) < 32 then
+      camera_x = max(0, camera_x - 1)
+    elseif camera_x + 120 - x < 32 then
+      camera_x = min(128, camera_x + 1)
+    end
+    if (y - camera_y) < 32 then
+      camera_y = max(0, camera_y - 1)
+    elseif camera_y + 120 - y < 32 then
+      camera_y = min(128, camera_y + 1)
+    end
+
     return tx, ty, tile_id
   end
 
   function me:update()
+    camera(camera_x, camera_y)
     local tx, ty, tile_id = move()
+    local key = tx .. "," .. ty
+    dialogue = dialogue_map[key]
+
+    if dialogue and btnp(4) then
+      return "dialogue", dialogue
+    end
   end
 
   function me:draw()
     cls(3)
-    rectfill(0, 0, 127, 7, 0)
-    rectfill(0, 8, 127, 27, 12)
+    rectfill(0, 0, 256, 7, 0)
+    rectfill(0, 8, 256, 27, 12)
     map()
     spr_outline(id, x, y, flip_x)
+
+    if dialogue then
+      local prompt = "\142 to enter"
+      local x = x - #prompt * 2 + 4
+      local y = y - 10
+      print(prompt, x, y, 7)
+    end
   end
 
   return me
